2bdca0df638f2d86713544394a5dc2234f3b45f8
shevcari000 <shevcari000@CAM-LAB-22.bgsd.k12.wa.us>
2019-10-08 07:49:09 -0700

    ok

--- src/Hourglass.java
+++ src/Hourglass.java
@@ -1,7 +1,14 @@
-/*
 
-*/
 
 public class Hourglass {
+	private static final int k = 0;
 
+	public static void main (String[] args, int k) {
+		for(int l=1; l<=8;l++)
+			for (int i=1; i<=l; i++)
+			System.out.println(":");
+			
+			System.out.println("");
+			            }
+	}
 }
