004098eca68f7dfac4937b4e326a82c4df517864
shevcari000 <shevcari000@CAM-LAB-22.bgsd.k12.wa.us>
2019-10-10 07:49:26 -0700

    Almost

--- src/Hourglass.java
+++ src/Hourglass.java
@@ -1,14 +1,35 @@
 
 
 public class Hourglass {
-	private static final int k = 0;
+	public static void main(String[] args) {
+		DrawTopLine();
+		DrawTop();
+		//Middle();
+		//DrawBottom();
+		//DrawBottomLine();
+		
+	}
+	
+	//Produces the first line
+	public static void DrawTopLine() {
+		System.out.print("|");
+		for (int i = 1; i <= 12; i++) {
+			System.out.print("\"");
+		}
+		System.out.println("|");
+	}
 
-	public static void main (String[] args, int k) {
-		for(int l =1; l<=8; l++) {
-			for (int i=1; i<=l;  i++) {
-				System.out.println(":"); 
-			
-					System.out.println("");
-			            }
+	//Produces the top half
+	public static void DrawTop() {
+		for (int line = 1; line <= 12; line++) {
+			System.out.print ("\\");
+			for (int i = 1; i <= (line - 1); i++) {
+				System.out.print (" ");
+			}
+		 for (int i=1; i <= (8-2 * line); i++) {
+		 }
+		}
 	}
 }
+
+		 
