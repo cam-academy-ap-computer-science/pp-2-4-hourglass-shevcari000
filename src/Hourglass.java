27d248101398f2d351e2a2e21d6b13b5b93570b2
shevcari000 <shevcari000@CAM-LAB-22.bgsd.k12.wa.us>
2019-10-15 07:47:35 -0700

    .

--- src/Hourglass.java
+++ src/Hourglass.java
@@ -21,11 +21,9 @@ public class Hourglass {
 
 	//Produces the top half
 	public static void DrawTop() {
-		for (int line = 1; line <= 12; line++) {
-			System.out.print ("\\");
-			for (int i = 1; i <= (line - 1); i++) {
-				System.out.print (" ");
-			}
+		for (int i = 1; i <= (line - 1); i++) {
+			System.out.print (" ");
+				
 		 for (int i=1; i <= (8-2 * line); i++) {
 		 }
 		}
